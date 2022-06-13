3a8c0b589baa84af587c3197ab9357f2cbcb2cd4
ThianReinoso <sebasreinoso@gmail.com>
2022-06-13 00:14:09 -0500

    Objects created

--- javadir/Main.java
+++ javadir/Main.java
@@ -3,5 +3,15 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
+        car.passegenger = 4;
+        car.printDataCar();
+
+        Car car2 = new Car("QWE567", new Account("Andres Herrera", "AND123"));
+        car2.license = "QWE567";
+        car2.driver = "Andrea Herrera";
+        car2.passegenger = 3;
+        car2.printDataCar();
     }
 }
